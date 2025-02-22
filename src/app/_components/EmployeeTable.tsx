5e9cbfa345a374a2a26884689a62cc885a16797e
sanskarchoudhry <sanskar22101@gmail.com>
2025-02-22 12:13:35 +0530

    feat: add row selection, fix styles

--- src/app/_components/EmployeeTable.tsx
+++ src/app/_components/EmployeeTable.tsx
@@ -1,29 +1,37 @@
 "use client";
-
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { MaterialReactTable, MRT_ColumnDef } from "material-react-table";
 import { Avatar, Box } from "@mui/material";
 import { Employee, employeeData } from "@/json-data";
 
 function EmployeeTable() {
+  const [expandedRows, setExpandedRows] = useState<{ [key: string]: boolean }>(
+    {}
+  );
+
+  const toggleExpand = (id: string) => {
+    setExpandedRows((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
+
   const columns = useMemo<MRT_ColumnDef<Employee>[]>(
     () => [
       {
-        accessorFn: (row) => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
-        id: "name", //id is still required when using accessorFn instead of accessorKey
+        accessorFn: (row) => `${row.firstName} ${row.lastName}`,
+        id: "name",
         header: "Name",
         size: 250,
         Cell: ({ renderedCellValue, row }) => (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
+          <Box sx={{ display: "flex", alignItems: "center", gap: "1rem" }}>
             <Avatar src={row.original.avatar} alt="Avatar" />
-            {/* using renderedCellValue instead of cell.getValue() preserves filter match highlighting */}
-            <span>{renderedCellValue}</span>
+            <Box>
+              <span className="font-medium text-gray-900">
+                {renderedCellValue}
+              </span>
+              <br />
+              <span className="text-sm text-gray-500">
+                @{row.original.username}
+              </span>
+            </Box>
           </Box>
         ),
       },
@@ -35,9 +43,11 @@ function EmployeeTable() {
           const status = row.original.status;
           return (
             <span
-              className={`
-            
-              py-1 px-2 rounded-[12px] border capitalize bg-blue-300 border-blue-800 text-blue-800`}
+              className={`py-1 px-2 rounded-[12px] border capitalize text-white ${
+                status === "working"
+                  ? "bg-blue-500 border-blue-800"
+                  : "bg-red-400 border-red-700"
+              }`}
             >
               {status}
             </span>
@@ -50,12 +60,54 @@ function EmployeeTable() {
         accessorKey: "team",
         header: "Teams",
         size: 200,
-        Cell: ({ row }) => row.original.team.join(", "),
+        Cell: ({ row }) => {
+          const teams = row.original.team;
+          const isExpanded = expandedRows[row.id]; // Track expanded state
+
+          return (
+            <Box display="flex" alignItems="center">
+              <div
+                className="flex gap-1 items-center"
+                onClick={() => toggleExpand(row.id)}
+              >
+                {(isExpanded ? teams : teams.slice(0, 3)).map((team, index) => {
+                  // Define a color intensity based on index
+                  const shade = 75 + index * 30; // Darkens with each entry
+
+                  return (
+                    <span
+                      key={index}
+                      className="py-1 px-2 rounded-[12px] border capitalize text-white"
+                      style={{
+                        backgroundColor: `rgb(59, 130, 246, ${shade / 100})`, // RGBA with increasing opacity
+                        borderColor: "rgb(37, 99, 235)", // Blue-600 border
+                      }}
+                    >
+                      {team}
+                    </span>
+                  );
+                })}
+                {!isExpanded && teams.length > 3 && (
+                  <span className="py-1 px-2 bg-slate-200 rounded-[25px] cursor-pointer">
+                    +{teams.length - 3}
+                  </span>
+                )}
+              </div>
+            </Box>
+          );
+        },
       },
     ],
-    []
+    [expandedRows]
   );
-  return <MaterialReactTable columns={columns} data={employeeData} />;
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={employeeData}
+      enableRowSelection
+    />
+  );
 }
 
 export default EmployeeTable;
--- src/json-data/index.ts
+++ src/json-data/index.ts
@@ -3,6 +3,7 @@ export type Employee = {
   lastName: string;
   email: string;
   avatar: string;
+  username: string;
   status: "working" | "not working";
   role: string;
   team: string[];
@@ -12,301 +13,338 @@ export const employeeData: Employee[] = [
   {
     firstName: "Dusty",
     lastName: "Kuvalis",
+    username: "dusty22",
     email: "Randy63@yahoo.com",
     role: "Chief Creative Technician",
     status: "working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
-    team: ["HR"],
+    team: ["Operations"],
   },
   {
     firstName: "D'angelo",
     lastName: "Moen",
+    username: "angel11",
     email: "Andrew88@hotmail.com",
     role: "Forward Response Engineer",
     status: "working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
-    team: ["Operations", "Engineering", "HR"],
+    team: ["Operations", "Sales", "Design", "HR", "Engineering", "Analytics"],
   },
   {
     firstName: "Devan",
     lastName: "Reinger",
+    username: "reiner",
     email: "Melissa_Lockman@hotmail.com",
     role: "Customer Intranet Consultant",
     status: "not working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
-    team: ["Sales", "Marketing", "Operations"],
+    team: ["AI"],
   },
   {
     firstName: "Leonardo",
     lastName: "Langworth",
+    username: "langardo",
     email: "Chadrick.Goldner87@gmail.com",
     role: "Senior Security Manager",
     status: "working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
-    team: ["Design", "HR"],
+    team: ["Engineering", "Sales", "Analytics", "AI", "Design"],
   },
   {
     firstName: "Douglas",
     lastName: "Denesik",
+    username: "bardock",
     email: "Dante.Deckow@hotmail.com",
     role: "Legacy Security Assistant",
     status: "not working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
-    team: ["Design", "Engineering", "HR", "Operations"],
+    team: ["AI", "Sales", "HR", "Operations"],
   },
   {
     firstName: "Jameson",
     lastName: "Mayer",
+    username: "droid4x",
     email: "Rosamond_Schuster@yahoo.com",
     role: "Regional Division Planner",
     status: "not working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
+    team: ["HR", "Sales", "Engineering", "Analytics"],
+  },
+  {
+    firstName: "Madaline",
+    lastName: "Quitzon",
+    username: "horizon",
+    email: "Alex_Grimes82@hotmail.com",
+    role: "Corporate Paradigm Strategist",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
+    team: ["Marketing", "Analytics", "Design", "Operations", "HR"],
+  },
+  {
+    firstName: "Dusty",
+    lastName: "Kuvalis",
+    username: "dusty22",
+    email: "Randy63@yahoo.com",
+    role: "Chief Creative Technician",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
+    team: ["HR", "Design", "Operations"],
+  },
+  {
+    firstName: "D'angelo",
+    lastName: "Moen",
+    username: "angel11",
+    email: "Andrew88@hotmail.com",
+    role: "Forward Response Engineer",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
+    team: ["Engineering", "Operations", "Design", "AI"],
+  },
+  {
+    firstName: "Devan",
+    lastName: "Reinger",
+    username: "reiner",
+    email: "Melissa_Lockman@hotmail.com",
+    role: "Customer Intranet Consultant",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
+    team: ["Analytics", "Sales", "AI", "Marketing"],
+  },
+  {
+    firstName: "Leonardo",
+    lastName: "Langworth",
+    username: "langardo",
+    email: "Chadrick.Goldner87@gmail.com",
+    role: "Senior Security Manager",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
+    team: ["Engineering", "Analytics", "Design"],
+  },
+  {
+    firstName: "Douglas",
+    lastName: "Denesik",
+    username: "bardock",
+    email: "Dante.Deckow@hotmail.com",
+    role: "Legacy Security Assistant",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
+    team: [
+      "Operations",
+      "Sales",
+      "Marketing",
+      "Analytics",
+      "HR",
+      "Engineering",
+    ],
+  },
+  {
+    firstName: "Jameson",
+    lastName: "Mayer",
+    username: "droid4x",
+    email: "Rosamond_Schuster@yahoo.com",
+    role: "Regional Division Planner",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
+    team: ["Engineering", "Design", "Marketing", "Sales", "HR"],
+  },
+  {
+    firstName: "Madaline",
+    lastName: "Quitzon",
+    username: "horizon",
+    email: "Alex_Grimes82@hotmail.com",
+    role: "Corporate Paradigm Strategist",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
+    team: ["Analytics", "Operations", "HR", "Marketing", "Sales"],
+  },
+  {
+    firstName: "Dusty",
+    lastName: "Kuvalis",
+    username: "dusty22",
+    email: "Randy63@yahoo.com",
+    role: "Chief Creative Technician",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
+    team: ["HR", "Engineering", "Marketing", "Operations"],
+  },
+  {
+    firstName: "D'angelo",
+    lastName: "Moen",
+    username: "angel11",
+    email: "Andrew88@hotmail.com",
+    role: "Forward Response Engineer",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
+    team: ["AI", "Sales", "Marketing", "Analytics"],
+  },
+  {
+    firstName: "Devan",
+    lastName: "Reinger",
+    username: "reiner",
+    email: "Melissa_Lockman@hotmail.com",
+    role: "Customer Intranet Consultant",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
+    team: ["Marketing", "Design", "AI"],
+  },
+  {
+    firstName: "Leonardo",
+    lastName: "Langworth",
+    username: "langardo",
+    email: "Chadrick.Goldner87@gmail.com",
+    role: "Senior Security Manager",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
+    team: ["Operations", "Marketing", "Analytics", "Design"],
+  },
+  {
+    firstName: "Douglas",
+    lastName: "Denesik",
+    username: "bardock",
+    email: "Dante.Deckow@hotmail.com",
+    role: "Legacy Security Assistant",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
+    team: ["Marketing", "Engineering", "Sales"],
+  },
+  {
+    firstName: "Jameson",
+    lastName: "Mayer",
+    username: "droid4x",
+    email: "Rosamond_Schuster@yahoo.com",
+    role: "Regional Division Planner",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
+    team: ["Analytics"],
+  },
+  {
+    firstName: "Madaline",
+    lastName: "Quitzon",
+    username: "horizon",
+    email: "Alex_Grimes82@hotmail.com",
+    role: "Corporate Paradigm Strategist",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
+    team: ["Engineering", "Sales", "Analytics", "Operations", "Marketing"],
+  },
+  {
+    firstName: "Dusty",
+    lastName: "Kuvalis",
+    username: "dusty22",
+    email: "Randy63@yahoo.com",
+    role: "Chief Creative Technician",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
+    team: ["Engineering", "Operations"],
+  },
+  {
+    firstName: "D'angelo",
+    lastName: "Moen",
+    username: "angel11",
+    email: "Andrew88@hotmail.com",
+    role: "Forward Response Engineer",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
+    team: ["AI", "Engineering", "Design"],
+  },
+  {
+    firstName: "Devan",
+    lastName: "Reinger",
+    username: "reiner",
+    email: "Melissa_Lockman@hotmail.com",
+    role: "Customer Intranet Consultant",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
+    team: ["Sales", "AI", "Marketing", "Engineering"],
+  },
+  {
+    firstName: "Leonardo",
+    lastName: "Langworth",
+    username: "langardo",
+    email: "Chadrick.Goldner87@gmail.com",
+    role: "Senior Security Manager",
+    status: "working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
+    team: ["Sales", "Analytics", "Design", "Operations"],
+  },
+  {
+    firstName: "Douglas",
+    lastName: "Denesik",
+    username: "bardock",
+    email: "Dante.Deckow@hotmail.com",
+    role: "Legacy Security Assistant",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
     team: ["Design"],
   },
   {
+    firstName: "Jameson",
+    lastName: "Mayer",
+    username: "droid4x",
+    email: "Rosamond_Schuster@yahoo.com",
+    role: "Regional Division Planner",
+    status: "not working",
+    avatar:
+      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
+    team: ["HR", "Analytics", "Engineering", "Design", "Sales"],
+  },
+  {
     firstName: "Madaline",
     lastName: "Quitzon",
+    username: "horizon",
     email: "Alex_Grimes82@hotmail.com",
     role: "Corporate Paradigm Strategist",
     status: "working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
-    team: ["Design", "HR", "Sales", "Operations"],
-  },
-  {
-    firstName: "Dusty",
-    lastName: "Kuvalis",
-    email: "Randy63@yahoo.com",
-    role: "Chief Creative Technician",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
-    team: ["Marketing", "Sales", "HR"],
-  },
-  {
-    firstName: "D'angelo",
-    lastName: "Moen",
-    email: "Andrew88@hotmail.com",
-    role: "Forward Response Engineer",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
     team: ["HR"],
   },
   {
-    firstName: "Devan",
-    lastName: "Reinger",
-    email: "Melissa_Lockman@hotmail.com",
-    role: "Customer Intranet Consultant",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
-    team: ["Design", "Sales", "Marketing"],
-  },
-  {
-    firstName: "Leonardo",
-    lastName: "Langworth",
-    email: "Chadrick.Goldner87@gmail.com",
-    role: "Senior Security Manager",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
-    team: ["HR", "Marketing", "Sales"],
-  },
-  {
-    firstName: "Douglas",
-    lastName: "Denesik",
-    email: "Dante.Deckow@hotmail.com",
-    role: "Legacy Security Assistant",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
-    team: ["HR"],
-  },
-  {
-    firstName: "Jameson",
-    lastName: "Mayer",
-    email: "Rosamond_Schuster@yahoo.com",
-    role: "Regional Division Planner",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
-    team: ["HR", "Sales", "Engineering"],
-  },
-  {
-    firstName: "Madaline",
-    lastName: "Quitzon",
-    email: "Alex_Grimes82@hotmail.com",
-    role: "Corporate Paradigm Strategist",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
-    team: ["HR", "Operations"],
-  },
-  {
     firstName: "Dusty",
     lastName: "Kuvalis",
+    username: "dusty22",
     email: "Randy63@yahoo.com",
     role: "Chief Creative Technician",
     status: "working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
-    team: ["HR", "Marketing", "Sales"],
+    team: ["Sales", "Design"],
   },
   {
     firstName: "D'angelo",
     lastName: "Moen",
+    username: "angel11",
     email: "Andrew88@hotmail.com",
     role: "Forward Response Engineer",
     status: "working",
     avatar:
       "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
-    team: ["HR", "Engineering", "Operations"],
-  },
-  {
-    firstName: "Devan",
-    lastName: "Reinger",
-    email: "Melissa_Lockman@hotmail.com",
-    role: "Customer Intranet Consultant",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
-    team: ["Sales", "HR"],
-  },
-  {
-    firstName: "Leonardo",
-    lastName: "Langworth",
-    email: "Chadrick.Goldner87@gmail.com",
-    role: "Senior Security Manager",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
-    team: ["Design", "Sales", "Engineering", "Operations"],
-  },
-  {
-    firstName: "Douglas",
-    lastName: "Denesik",
-    email: "Dante.Deckow@hotmail.com",
-    role: "Legacy Security Assistant",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
-    team: ["Sales"],
-  },
-  {
-    firstName: "Jameson",
-    lastName: "Mayer",
-    email: "Rosamond_Schuster@yahoo.com",
-    role: "Regional Division Planner",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
-    team: ["Marketing"],
-  },
-  {
-    firstName: "Madaline",
-    lastName: "Quitzon",
-    email: "Alex_Grimes82@hotmail.com",
-    role: "Corporate Paradigm Strategist",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
-    team: ["Sales", "Engineering"],
-  },
-  {
-    firstName: "Dusty",
-    lastName: "Kuvalis",
-    email: "Randy63@yahoo.com",
-    role: "Chief Creative Technician",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
-    team: ["Marketing", "Design", "HR"],
-  },
-  {
-    firstName: "D'angelo",
-    lastName: "Moen",
-    email: "Andrew88@hotmail.com",
-    role: "Forward Response Engineer",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
-    team: ["Marketing", "Design", "HR"],
-  },
-  {
-    firstName: "Devan",
-    lastName: "Reinger",
-    email: "Melissa_Lockman@hotmail.com",
-    role: "Customer Intranet Consultant",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
-    team: ["Design", "HR", "Engineering"],
-  },
-  {
-    firstName: "Leonardo",
-    lastName: "Langworth",
-    email: "Chadrick.Goldner87@gmail.com",
-    role: "Senior Security Manager",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/639.jpg",
-    team: ["Marketing"],
-  },
-  {
-    firstName: "Douglas",
-    lastName: "Denesik",
-    email: "Dante.Deckow@hotmail.com",
-    role: "Legacy Security Assistant",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/394.jpg",
-    team: ["Marketing", "Operations"],
-  },
-  {
-    firstName: "Jameson",
-    lastName: "Mayer",
-    email: "Rosamond_Schuster@yahoo.com",
-    role: "Regional Division Planner",
-    status: "not working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg",
-    team: ["Design", "Operations", "Engineering"],
-  },
-  {
-    firstName: "Madaline",
-    lastName: "Quitzon",
-    email: "Alex_Grimes82@hotmail.com",
-    role: "Corporate Paradigm Strategist",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
-    team: ["Operations", "Design", "HR", "Marketing"],
-  },
-  {
-    firstName: "Dusty",
-    lastName: "Kuvalis",
-    email: "Randy63@yahoo.com",
-    role: "Chief Creative Technician",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg",
-    team: ["Marketing"],
-  },
-  {
-    firstName: "D'angelo",
-    lastName: "Moen",
-    email: "Andrew88@hotmail.com",
-    role: "Forward Response Engineer",
-    status: "working",
-    avatar:
-      "https://ipfs.io/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg",
-    team: ["Operations", "Engineering", "Sales", "Marketing"],
+    team: ["Marketing", "Design", "AI"],
   },
 ];
